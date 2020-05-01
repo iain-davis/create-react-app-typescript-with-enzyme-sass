51f10817f2356f526d75a65765cc16eb18fd2fba
Iain E. Davis <iain@iaindavis.name>
2020-05-01 12:47:32 -0600

    write a basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import App from './App'
+import {shallow} from 'enzyme'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+    it('renders', () => {
+        const app = shallow(<App/>)
+
+        expect(app).not.toBeEmptyRender()
+    })
+})
